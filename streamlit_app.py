377df97f8d7c066cb44589f0c9d601688a3bf994
RFahy7 <reegan.fahy7@mail.dcu.ie>
2024-12-08 18:33:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,51 @@
 import streamlit as st
-st.write("Hello world")
+import numpy as np
+import joblib
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk import download
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LinearRegression
+
+# Download any required NLTK data
+download('stopwords')
+download('vader_lexicon')
+download('wordnet')
+
+# Load the model and vectorizer
+linear_regressor = joblib.load('linear_regressor.joblib')
+vectorizer = joblib.load('vectorizer.joblib')
+
+# Define text preprocessing function
+lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
+
+
+def preprocess_text(headline):
+    headline = ''.join([c for c in headline if c not in ('!', '.', ':', ',', '?', '(', ')', '"')])
+    headline = ' '.join([lemmatizer.lemmatize(word) for word in headline.split() if word not in stop_words])
+    return headline
+
+
+# Define the Streamlit app
+def main():
+    st.title("Headline Sentiment Prediction")
+
+    st.header("Enter a News Headline")
+    user_input = st.text_input("Headline", "Type your headline here...")
+
+    if st.button("Predict Sentiment"):
+        # Preprocess the input headline
+        processed_input = preprocess_text(user_input)
+
+        # Transform the input text to the TF-IDF vector
+        input_vector = vectorizer.transform([processed_input])
+
+        # Make prediction using the linear regression model
+        sentiment_prediction = linear_regressor.predict(input_vector)
+
+        st.write(f"Predicted Sentiment Score: {sentiment_prediction[0]:.2f}")
+
+
+if __name__ == "__main__":
+    main()
